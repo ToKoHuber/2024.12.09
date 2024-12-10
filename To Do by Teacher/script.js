dc0c3865b54e8fdc807cef9cf692b0138d195017
24LP2879 24LP2879 <24LP2879@G304-CC0235.local>
2024-12-10 10:18:27 +0800

    dec 10nd first commit

--- To Do by Teacher/script.js
+++ To Do by Teacher/script.js
@@ -1,45 +1,131 @@
+// const containerItems = [
+//   {
+//     title: "To do",
+//     count: 5,
+//     color: "white",
+//   },
+//   {
+//     title: "In progress",
+//     count: 5,
+//     color: "white",
+//   },
+//   {
+//     title: "Done",
+//     count: 5,
+//     color: "green",
+//   },
+//   {
+//     title: "Blocked",
+//     count: 5,
+//     color: "red",
+//   },
+// ];
+
+// function addTaskList(title, color, count) {
+//   const taskContainer2 = document.querySelector("#taskContainer");
+//   const todoList = document.createElement("div");
+//   todoList.setAttribute("class", "todoList");
+//   const titleDiv = document.createElement("div");
+//   titleDiv.setAttribute("class", "title");
+//   todoList.appendChild(titleDiv);
+//   const h2 = document.createElement("h2");
+//   h2.innerText = title;
+//   const cirlce = document.createElement("div");
+//   cirlce.setAttribute("class", "circle");
+//   cirlce.style.backgroundColor = color;
+//   titleDiv.appendChild(cirlce);
+//   titleDiv.appendChild(h2);
+//   taskContainer2.appendChild(todoList);
+// }
+
+// addTaskList(
+//   containerItems[0].title,
+//   containerItems[0].color,
+//   containerItems[0].count
+// );
+
+//code by bilgvvn bagsh
+
 const containerItems = [
   {
     title: "To do",
     count: 5,
     color: "white",
+    id: "todo",
   },
   {
     title: "In progress",
     count: 5,
     color: "white",
+    id: "in-progress",
   },
   {
     title: "Done",
     count: 5,
     color: "green",
+    id: "done",
   },
   {
     title: "Blocked",
     count: 5,
     color: "red",
+    id: "blocked",
+  },
+];
+const todoData = [
+  {
+    title: "Geree tseverleh",
+    date: "2024-12-09",
+    state: "todo",
+  },
+  {
+    title: "Shine jildee yvah",
+    date: "2024-12-09",
+    state: "todo",
+  },
+  {
+    title: "Huvtssaa beldeh",
+    date: "2024-12-09",
+    state: "todo",
   },
 ];
 
-function addTaskList(title, color, count) {
+function addTaskList(title, color, count, id) {
+  //html div iig bariad avsan
   const taskContainer2 = document.querySelector("#taskContainer");
+
+  //todoList = <div class="todoList">
+  //   <div class="title"></div>
+  // </div>
   const todoList = document.createElement("div");
   todoList.setAttribute("class", "todoList");
+  todoList.setAttribute("id", id);
+
+  // title = <div class="title"></div>
   const titleDiv = document.createElement("div");
   titleDiv.setAttribute("class", "title");
+
   todoList.appendChild(titleDiv);
+  // <h2>To do</h2>
   const h2 = document.createElement("h2");
   h2.innerText = title;
+
+  // <div style={backgroundColor:"white" } class="circle"></div>
   const cirlce = document.createElement("div");
   cirlce.setAttribute("class", "circle");
   cirlce.style.backgroundColor = color;
+
+  // <p class="count"></p>
+  const para = document.createElement("p");
+  para.setAttribute("class", "count");
+  para.innerText = count;
+
   titleDiv.appendChild(cirlce);
   titleDiv.appendChild(h2);
+  titleDiv.appendChild(para);
   taskContainer2.appendChild(todoList);
 }
 
-addTaskList(
-  containerItems[0].title,
-  containerItems[0].color,
-  containerItems[0].count
-);
+containerItems.map((item) => {
+  addTaskList(item.title, item.color, item.count, item.id);
+});
--- naruto/script-naruto.js
+++ naruto/script-naruto.js
@@ -8,20 +8,35 @@
         <img src="https://cdn.staticneo.com/w/naruto/Nprofile2.jpg" alt="" />
       </div> 
 }*/
-const narutoInfo = [
-  {
-    title: "Naruto",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,dignissimos?",
-    imgUrl: "https://cdn.staticneo.com/w/naruto/Nprofile2.jpg",
-  },
-];
+// const narutoInfo = [
+//   {
+//     title: "Naruto",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,dignissimos?",
+//     imgUrl: "https://cdn.staticneo.com/w/naruto/Nprofile2.jpg",
+//   },
+// ];
 const appDiv = document.querySelector(".app");
 
 function generateCard(title, description, imgUrl) {
   const cardDiv = document.createElement("div");
+  const h2tag = document.createElement("h2");
+  h2tag.innerText = title;
+  const textTag = document.createElement("p");
+  textTag.innerText = description;
+
+  const imgTag = document.createElement("img");
+  imgTag.setAttribute("src", imgUrl);
+
   cardDiv.setAttribute("class", "card");
+  cardDiv.appendChild(h2tag);
+  cardDiv.appendChild(textTag);
+  cardDiv.appendChild(imgTag);
   appDiv.appendChild(cardDiv);
 }
 
-generateCard(narutoInfo);
+generateCard(
+  "Naruto",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,dignissimos?",
+  "https://cdn.staticneo.com/w/naruto/Nprofile2.jpg"
+);
